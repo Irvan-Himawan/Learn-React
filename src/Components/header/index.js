016c2a9cd9fab8710c54dd98188874f3c8518065
Irvan Himawan <himawan.alvisa@gmail.com>
2020-09-16 22:20:41 +0700

    add scss

--- src/Components/header/index.js
+++ src/Components/header/index.js
@@ -4,15 +4,20 @@ import { Link } from "react-router-dom";
 function header() {
   return (
     <header>
+      <h1>
+        <Link to="/">LOGO</Link>
+      </h1>
       <nav>
-        <li>
-          {" "}
-          <Link to="/counter">Counter</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/todolist">To-do-list</Link>
-        </li>
+        <ul>
+          <li>
+            {" "}
+            <Link to="/counter">Counter</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/todolist">To-do-list</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
